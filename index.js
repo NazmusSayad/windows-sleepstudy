3d29951859f8eebc2f8f62b0c756134ddc9495f7
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-10-18 16:21:37 +0600

    M index.html  M index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+
+const files = fs
+  .readdirSync(__dirname)
+  .filter(
+    (file) =>
+      file !== 'index.js' && file !== 'index.html' && file.endsWith('.html')
+  )
+
+fs.writeFileSync(
+  path.join(__dirname, 'index.html'),
+  files.map((file) => `<a href="/${file}">${file}</a>`).join('<br>')
+)
+
+console.log(files)
